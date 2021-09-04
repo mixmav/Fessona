bd73a0b7088ee652661b4150134c9c0c4a46239c
Manav <manav.sg1@gmail.com>
2021-09-04 15:03:03 +1000

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\models\Site;
+// use App\models\Site;
 
 class SiteController extends Controller
 {
 	public function GetActiveSites(){
-		return Site::all()->where('active', true);
+		// return Site::all()->where('active', true);
+		return "hi";
 	}
 }
